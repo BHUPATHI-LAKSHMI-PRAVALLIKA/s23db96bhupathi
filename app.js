f8f6a54ae0a5bb7df192361c2b7f39aa839578f7
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2023-11-04 13:13:23 -0500

    use a good message

--- app.js
+++ app.js
@@ -1,48 +1,129 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+var Costume = require("./models/costume");
+var resourceRouter = require("./routes/resource");
+async function recreateDB() {
+  // Delete everything
+  await Costume.deleteMany();
+  let costume1 = new Costume({
+    costume_type: "ghost",
+    size: "large",
+    cost: 15.4,
+  });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productRouter = require('./routes/product');
-var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
+  let costume2 = new Costume({
+    costume_type: "witch",
+    size: "medium",
+    cost: 13.5,
+  });
+
+  let costume3 = new Costume({
+    costume_type: "demon",
+    size: "extra large",
+    cost: 11.5,
+  });
+
+
+
+  costume1
+    .save()
+    .then((doc) => {
+      console.log("First object saved");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    costume2
+    .save()
+    .then((doc) => {
+      console.log("Second object saved");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    costume3
+    .save()
+    .then((doc) => {
+      console.log("Third object saved");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+
+
+
+}
+
+let reseed = true;
+if (reseed) {
+  recreateDB();
+}
+
+require("dotenv").config();
+const connectionString = process.env.MONGO_CON;
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => console.log("Connected to MongoDB"))
+
+  .catch((err) => console.error("Error connecting to MongoDB: ", err));
+
+var db = mongoose.connection;
+//Bind connection to error event
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+});
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var productRouter = require("./routes/product");
+var boardRouter = require("./routes/board");
+var chooseRouter = require("./routes/choose");
 
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/product', productRouter);
-app.use('/board',boardRouter);
-app.use('/choose',chooseRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/product", productRouter);
+app.use("/board", boardRouter);
+app.use("/choose", chooseRouter);
+app.use("/resource",resourceRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
