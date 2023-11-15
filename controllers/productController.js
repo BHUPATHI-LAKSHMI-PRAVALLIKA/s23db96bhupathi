7474d587edce7c65104e6d84852e2edec515d4d4
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2023-11-15 00:44:58 -0600

    files updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -122,3 +122,14 @@ exports.product_update_Page = async function (req, res) {
     res.send(`{'error': '${err}'}`);
   }
 };
+// Handle a delete one view with id from query
+exports.product_delete_Page = async function (req, res) {
+  console.log("Delete view for id " + req.query.id);
+  try {
+    result = await Product.findById(req.query.id);
+    res.render("productdelete", { title: "Product Delete", toShow: result });
+  } catch (err) {
+    res.status(500);
+    res.send(`{'error': '${err}'}`);
+  }
+};
--- routes/products.js
+++ routes/products.js
@@ -12,6 +12,8 @@ router.get('/detail', product_controlers.product_view_one_Page);
 router.get('/create', product_controlers.product_create_Page)
 /* GET create update page */
 router.get('/update', product_controlers.product_update_Page);
+/* GET delete product page */
+router.get('/delete', product_controlers.product_delete_Page);
 
 module.exports = router;
 
