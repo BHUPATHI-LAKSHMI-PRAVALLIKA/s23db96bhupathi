1cbe789ebbe0a9d8a1e52aa6b05d627d061c1973
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2023-11-29 19:40:04 -0600

    files updated

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const productSchema = mongoose.Schema({
-product_type: String,
-feature: String,
+product_type: {type:String,required:true},
+feature: {type:String,required:true},
 cost: {type:Number,required:true,min:0,max:10000}
 })
 module.exports = mongoose.model("Product",productSchema);
--- routes/products.js
+++ routes/products.js
@@ -1,32 +1,28 @@
 var express = require("express");
-const product_controlers = require("../controllers/productController");
-var router = express.Router();
+const product_controllers = require("../controllers/productController");
 var passport = require("passport");
-/* GET products */
-router.get("/", product_controlers.product_view_all_Page);
-// GET request for one product.
-router.get("/products/:id", product_controlers.product_detail);
-router.put("/products/:id", product_controlers.product_update_put);
-/* GET detail product page */
-router.get("/detail", product_controlers.product_view_one_Page);
-/* GET create product page */
-router.get("/create", product_controlers.product_create_Page);
-/* GET create update page */
-// A little function to check if we have an authorized user and continue on
-// redirect to login.
+var router = express.Router();
 const secured = (req, res, next) => {
   if (req.user) {
     return next();
   }
   res.redirect("/login");
 };
-/* GET update product page */
-router.get("/update", secured, product_controlers.product_update_Page);
+// GET products
+router.get("/", product_controllers.product_view_all_Page);
+// GET request for one product.
+router.get("/products/:id", secured, product_controllers.product_detail);
+/* GET detail product page */
+router.get("/detail", secured, product_controllers.product_view_one_Page);
+/* GET create product page */
+router.get("/create", secured, product_controllers.product_create_Page);
+/* GET create update page */
+router.get("/update", secured, product_controllers.product_update_Page);
+/* GET delete product page */
+router.get("/delete", secured, product_controllers.product_delete_Page);
 router.post("/login", passport.authenticate("local"), function (req, res) {
+  if (req.session.returnTo) (res.redirect(req.session.returnTo));
   res.redirect("/");
 });
-
-/* GET delete product page */
-router.get("/delete", product_controlers.product_delete_Page);
 
 module.exports = router;
